247593f057473aca8e7b3857c4d348d6efc201d2
Mario E Valdes-Tresanco <marioe911116@gmail.com>
2026-03-17 18:29:09 -0600

    fix: Adjust legend position in quality control graph for better visibility

--- app/dashboards/dashboards/dashboard/quality_control.py
+++ app/dashboards/dashboards/dashboard/quality_control.py
@@ -897,7 +897,7 @@ def callbacks(app):
                     legend=dict(
                         orientation="h",
                         yanchor="bottom",
-                        y=1.02,
+                        y=1.08,
                         xanchor="left",
                         x=0,
                         bgcolor="rgba(0,0,0,0)",
